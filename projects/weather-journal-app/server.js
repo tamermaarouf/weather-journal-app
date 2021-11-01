22564889a384a263d7886f96c11ae24418b01ff7
tamer_maarouf <tamermaarouf@gmail.com>
2021-11-01 06:13:40 +0200

    run server on port 3000

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -6,14 +6,20 @@ const express = require('express');
 // Start up an instance of app
 const app = express();
 /* Middleware*/
+const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+app.listen(3000, ()=> {
+    console.log('Your server start on port 3000');
+})
