f2eca347b6fe476a79980061f9221a1941a794da
tamer_maarouf <tamermaarouf@gmail.com>
2021-11-02 04:39:39 +0200

    get Weather Data

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,6 +1,19 @@
 /* Global Variables */
 const apiKey = 'a61a89e4fe9e4551a5e2b91fb1180525';
-const fullURL = `api.openweathermap.org/data/2.5/weather?zip={zip code}&appid={apiKey}&units=metric`
+
+const generateBtn = document.querySelector('#generate');
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+generateBtn.addEventListener('click', async () => {
+    const zipCode =document.getElementById('zip').value;
+    const fullURL = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=metric`;
+    const res = await fetch(fullURL);
+    try {
+        const resData = await res.json();
+        const temp = resData.main.temp;
+    } catch (error) {
+        console.log('error', error);
+    }
+})
