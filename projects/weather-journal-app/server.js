bcf0b9d98e2f27795deb1e8752ad07c9c6708ebd
tamer_maarouf <tamermaarouf@gmail.com>
2021-11-04 06:17:36 +0200

    update UI

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -3,10 +3,13 @@ projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
+
 // Start up an instance of app
 const app = express();
+
 /* Middleware*/
 const bodyParser = require('body-parser');
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -19,8 +22,32 @@ app.use(cors());
 app.use(express.static('website'));
 
 
-// Setup Server
-app.listen(3000, ()=> {
-    console.log('Your server start on port 3000');
-});
+// Spin up the server
+const port = 3000;
+const server = app.listen(port, listening);
+
+// Callback to debug
+ function listening(){
+    // console.log(server);
+    console.log(`running on localhost: ${port}`);
+  };
+
+  // Initialize all route with a callback function
+  // GET route
+  app.get('/updateUiTemp', sendData);
+
+// Callback function to complete GET '/all'
+function sendData (request, response) {
+  console.log(projectData);
+  // response.send(projectData);
+}
+
+// Post Route
+app.post('/addTemperature', addTemperature);
+
+function addTemperature (req,res){
+  projectData = {...req.body};
+  res.send(projectData);
+  // console.log(projectData);
+};
 
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,20 +1,75 @@
 /* Global Variables */
-const apiKey = 'a61a89e4fe9e4551a5e2b91fb1180525';
-
 const generateBtn = document.querySelector('#generate');
+
+// Personal API Key for OpenWeatherMap API
+const apiKey = 'a61a89e4fe9e4551a5e2b91fb1180525';
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?'
+
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-generateBtn.addEventListener('click', async () => {
-    const zipCode =document.getElementById('zip').value;
-    const fullURL = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=metric`;
+// Event listener to add function to existing HTML DOM element
+generateBtn.addEventListener('click', performAction);
+
+/* Function called by event listener */
+function performAction(e){
+    const zipCode = document.getElementById('zip').value;
+    const feeling = document.querySelector('#feelings').value
+    getTempAPI(baseURL, zipCode, apiKey)
+    .then(function(data){
+        // console.log('.then', data);
+        postData('/addTemperature', {date: newDate, Temp: data, content: feeling})
+        updateUI()  
+    })
+}
+
+/* Function to GET Web API Data*/
+const getTempAPI = async (baseURL, zipCode, apiKey)=>{
+    const fullURL = `${baseURL}zip=${zipCode}&appid=${apiKey}&units=metric`;
+    // console.log(fullURL);
     const res = await fetch(fullURL);
     try {
-        const resData = await res.json();
-        const temp = resData.main.temp;
-        console.log(temp);
+        const data = await res.json();
+        const dataTemp = data.main.temp;
+        // console.log(dataTemp);
+        return dataTemp;
     } catch (error) {
         console.log('error', error);
     }
-})
+}
+
+/* Function to POST data */
+const postData = async ( url = '', data = {})=>{
+    // console.log('server = ', data)
+      const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data), // body data type must match "Content-Type" header        
+    });
+  
+      try {
+        const newData = await response.json();
+        // console.log('newData', newData);
+        return newData
+      }catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+      }
+  }
+
+  const updateUI = async () =>{
+      const req = await fetch('/updateUiTemp');
+      try {
+          const dataUI = await req.json();
+          document.getElementById('date').innerHTML = 'Date:' + ' ' + dataUI.date;
+          document.getElementById('temp').innerHTML = 'Temperature:' + ' ' + dataUI.Temp;
+          document.getElementById('content').innerHTML = 'you feeling today : ' + ' ' + dataUI.content;
+          console.log(dataUI);
+      } catch (error) {
+          
+      }
+  }
